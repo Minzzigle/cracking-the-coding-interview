a8d12871a3f95175193fcdd4e08d48fd6c4b3484
astrod <cha452jg@gmail.com>
2017-05-24 01:40:39 +0900

    change packagne name

--- src/main/java/pattern/JU/interpreter/Node.java
+++ src/main/java/pattern/JU/interpreter/Node.java
@@ -4,5 +4,5 @@ package pattern.JU.interpreter;
  * Created by cha45 on 2017-05-21.
  */
 public abstract class Node {
-    public abstract void parse(Context context) throws ParseException, pattern.JU.interpreter.ParseException;
+    public abstract void parse(Context context) throws ParseException;
 }
